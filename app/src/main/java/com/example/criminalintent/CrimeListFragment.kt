686c7efbaf2b69dda07ce1e732aba2deb3f6250b
Lakshita Ganesh Kumar <lakshita15@outlook.com>
2026-03-23 09:49:25 -0400

    mar 23 class - i need to catch up from the friday before spring break

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -2,16 +2,40 @@ package com.example.criminalintent
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.savedstate.serialization.saved
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
     private val crimeListViewModel : CrimeListViewModel by viewModels()
+    private var _binding : FragmentCrimeDetailBinding? = null
+    private val binding
+        get() = checkNotNull(_binding){"We got an error in the binding"}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes = ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        val crimes = crimeListViewModel.crimes
+        val adapter = CrimeListAdapter(crimes)
+        binding.crimeRecyclerView.adapter = adapter
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
