00359b2999ae1f1a151bc2ec37b1a88f5e01b753
Lakshita Ganesh Kumar <lakshita15@outlook.com>
2026-04-01 11:11:34 -0400

    end of class 4/1/2026. start of HW4

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -7,12 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
     private val crimeListViewModel : CrimeListViewModel by viewModels()
+    private var job: Job? = null // you will delete this
     private var _binding : FragmentCrimeListBinding? = null
     private val binding
         get() = checkNotNull(_binding){"We got an error in the binding"}
@@ -33,6 +37,20 @@ class CrimeListFragment : Fragment() {
         val adapter = CrimeListAdapter(crimes)
         binding.crimeRecyclerView.adapter = adapter
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        job = viewLifecycleOwner.lifecycleScope.launch {
+            val crimes = crimeListViewModel.loadCrimes()
+            binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // cancel the job started in onStart
+        job?.cancel()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -1,21 +1,51 @@
 package com.example.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
 
+private const val TAG = "CrimeListViewModel"
+
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
+    var crimes = mutableListOf<Crime>()
     init{
-        for(num in 0 until 100) {
+        Log.d(TAG, "About to start a coroutine")
+        viewModelScope.launch {
+            Log.d(TAG, "Coroutine launched")
+            crimes += loadCrimes()
+//            delay(5000)
+//            for (num in 0 until 100) {
+//                val crime = Crime(
+//                    id = UUID.randomUUID(),
+//                    title = "Crime #${num + 1}",
+//                    date = Date(),
+//                    isSolved = num % 2 == 1
+//                )
+//                crimes += crime
+//                //crimes.add(crime)
+//            }
+            Log.d(TAG, "Loading crimes finished")
+            Log.d(TAG, "Out of VM Scope")
+            Log.d(TAG, "Size of crime ${crimes.size}")
+        }
+    }
+
+    suspend fun loadCrimes(): List<Crime> {
+        val result = mutableListOf<Crime>()
+        delay(5000)
+        for (num in 0 until 100) {
             val crime = Crime(
                 id = UUID.randomUUID(),
                 title = "Crime #${num + 1}",
                 date = Date(),
                 isSolved = num % 2 == 1
             )
-            crimes += crime
-            //crimes.add(crime)
+            result += crime
         }
+        return result
     }
 }
